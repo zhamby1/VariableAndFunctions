d03a420c9d3eca048c00f8a762379609111010ea
Zach Hamby <zhamby1@ivytech.edu>
2019-09-12 14:12:49 -0400

    Created a button that changes text

---
+++ GodotFunctions/Panel.gd
@@ -0,0 +1,11 @@
+extends Panel
+
+
+func _ready():
+	get_node("Button").connect("pressed",self,"_on_Button_pressed")
+	
+
+
+
+func _on_Button_pressed():
+	get_node("Label").text = "My Text Changed!"
